2698515fca36dd2e6ac315b85f460da13f72b63a
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-09-11 12:13:25 +0000

    Fix: correct brackets to curly brackets around gameType

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoad", function(){
-    let buttons = documnet.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function(){
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clisked submit!")
-            }else{
+                alert(`You clicked submit!`)
+            } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked $(gameType)`);
+                alert(`You clicked ${gameType}`);
             }
         })
     }
